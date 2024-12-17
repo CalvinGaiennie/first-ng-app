c09f44084e83ae130941a824b1adaf9252f911e4
CalvinGaiennie <calvingaiennie@gmail.com>
2024-12-17 13:49:41 -0600

    started angular crash course code with ahsan

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
-  styles: [],
+  imports: [RouterOutlet, HomeComponent, HeaderComponent],
+  template: `<app-header /> <app-home />`,
+  styles: [
+    `
+      p {
+        color: red;
+      }
+    `,
+  ],
 })
 export class AppComponent {
   title = 'first-ng-app';
